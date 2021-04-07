df216a0c837702992296e6e459c53b57321266d2
NiKo <80517411+NikitaKJava@users.noreply.github.com>
2021-04-07 14:43:41 +0200

    Add files via upload
    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nikita on 15.03.2021 at 23:03
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        TicTacToe ticTacToe = new TicTacToe();
+
+    }
+}
